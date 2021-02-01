0530d607e8811ef5df15c22cc98da8855c951b95
Manuel Garcia <gilbert.mg22@gmail.com>
2021-02-01 09:36:37 +0100

    .

--- badge_hub/settings.py
+++ badge_hub/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'mj+utd67b=p9--(t+0v@l4ahh5=9jk)pkcwl*-%x#dd!+njbpa'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
